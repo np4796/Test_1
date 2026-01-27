eac9a6bb428fe3e7418c876b3f939d3a39792ccb
np4796 <103492075+np4796@users.noreply.github.com>
2026-01-27 12:28:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ function getApiKey() {
 }
 
 console.log("API Key:", getApiKey());
+"new_scan",
+"new_scan",
+"443f7098cc8793e27c172954bf28bd02d995abcb67c9247e9e960e0163d3eb9c"
+"now"
+"11b8cb112292f87d6a9eee6d4989bff33c"
+"ZEqCS4GqM2ep5TZZOhWYiFM7GSVXgXXsEO7EzseE"
