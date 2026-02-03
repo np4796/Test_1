4fdc366a0abb0ce90a68f34fe06f6fe019a53441
np4796 <103492075+np4796@users.noreply.github.com>
2026-02-03 12:45:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 function getApiKey() {
     // WARNING: Hardcoded API key for testing secret detection
-    const REDACTED_SECRET2 = REDACTED_SECRETjmeter;
+    const REDACTED_SECRET2 = jmeter_secret;
     return API_KEY;
 }
 
