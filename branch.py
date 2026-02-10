c52c1b661655549d2461407706f65b8e3b61d8f8
np4796 <103492075+np4796@users.noreply.github.com>
2026-02-10 17:51:55 +0530

    Create branch.py

---
+++ branch.py
@@ -0,0 +1,5 @@
+function getApiKey() {
+    // WARNING: Hardcoded API key for testing secret detection
+    const REDACTED_SECRET2 =  ghp_SSvpBCLl7Ztop36uzdqGZuJEEDrDrF4t4RYg;
+    return API_KEY;
+}
