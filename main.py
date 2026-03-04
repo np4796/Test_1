90b4c6e5ab476131e2b1d3a10c0ed44b33673a61
np4796 <103492075+np4796@users.noreply.github.com>
2026-03-04 11:17:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 # main.py - Example Python file with a hardcoded secret
 
 def get_secret():
-    # WARNING: Hardcoded secret for testing secret detection
-    REDACTED_SECRET2
-API_KEY: "REDACTED_SECRET
-    return SECRET_KEY
+new_scan
+new_scan
+new_scan
+Klg8Q~T~tAex0UTW7gI1yd5gV6083SgF~c.
+new_scan
+now
+AKIAXYKJWHA8HODGYIAV
+ ZEqCS4GqM3ep7TZZOhWYiFM7GSVXgXXsEO6EzseE
+Region – us-west-2
+{
+
+"new_scan",
+"new_scan",
+new_scan,
+    "new_scan
+"new_scan"
+
+"new_scan",
+"new_scan",
+new_scan,
+"new_scan"
+new_scan
+ now
+new_scan,
+: "new_scan
+new_scan
+ ZEqCS4GqM3ep7TZZOhWYiFM7GSVXgXXsEO6EzseE
+ "ATATT7xFfGF0b0aMRGgGvHyPpG8DrigLkoZAybP5gaG_fGChgSH0LY78OuuWFJugG7Y_f2SM1CxNFwMJoMULY6XYHoXZlN0feArwtDLEqdNFI4CQSgfgKYXtXAa76wxGMW0YkgcupRtEeH36Zg-ybCYQ28Cbm9LlDgPRFNy5ausm-8JH0-q3QAw=74BD57D9",
+new_scan
 
 if __name__ == "__main__":
     print("The secret is:", get_secret())
